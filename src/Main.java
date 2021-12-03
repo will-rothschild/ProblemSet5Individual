8180ce9297502834f91b79c574dd15e2966f90e6
willrothschild <rothwg21@wfu.edu>
2021-12-03 14:03:10 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Move visibility true to bottom of code
+        JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+        obj.setVisible(true);
+    }
+
+
+
+}
